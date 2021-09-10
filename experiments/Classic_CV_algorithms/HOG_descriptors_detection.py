d2a35d9a67337a9869a17fb4a39d8a5d2425f466
Glebbs <glebrost2013@yandex.ru>
2021-09-10 19:24:38 +0300

    add HOG and SIFT

--- experiments/Classic_CV_algorithms/HOG_descriptors_detection.py
+++ experiments/Classic_CV_algorithms/HOG_descriptors_detection.py
@@ -1,1 +1,77 @@
+import cv2
+import numpy as np
+
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import matplotlib as plt
+
+
+################################## HOG + SVM ############################
+
+def convert_and_trim_bb(image, rect):
+    # extract the starting and ending (x, y)-coordinates of the
+    # bounding box
+    startX = rect.left()
+    startY = rect.top()
+    endX = rect.right()
+    endY = rect.bottom()
+    # ensure the bounding box coordinates fall within the spatial
+    # dimensions of the image
+    startX = max(0, startX)
+    startY = max(0, startY)
+    endX = min(endX, image.shape[1])
+    endY = min(endY, image.shape[0])
+    # compute the width and height of the bounding box
+    w = endX - startX
+    h = endY - startY
+    # return our bounding box coordinates
+    return (startX, startY, w, h)
+
+
+# load dlib's HOG + Linear SVM face detector
+print("[INFO] loading HOG + Linear SVM face detector...")
+detector = dlib.get_frontal_face_detector()
+# load the input image from disk, resize it, and convert it from
+# BGR to RGB channel ordering (which is what dlib expects)
+image = cv2.imread('C:/Users/glebr/Desktop/pict_2.jpg')
+image = imutils.resize(image, width=600)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# perform face detection using dlib's face detector
+start = time.time()
+print("[INFO[ performing face detection with dlib...")
+rects = detector(rgb, 1)
+end = time.time()
+print("[INFO] face detection took {:.4f} seconds".format(end - start))
+
+# convert the resulting dlib rectangle objects to bounding boxes,
+# then ensure the bounding boxes are all within the bounds of the
+# input image
+boxes = [convert_and_trim_bb(image, r) for r in rects]
+# loop over the bounding boxes
+for (x, y, w, h) in boxes:
+    # draw the bounding box on our image
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
+
+######################################### SIFT ####################################
+
+import numpy as np
+import cv2 as cv
+# img = cv.imread('C:/Users/glebr/Desktop/pict_5.jpg')
+# gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# sift = cv.SIFT_create()
+# kp = sift.detect(gray,None)
+# img=cv.drawKeypoints(gray,kp,img)
+# cv.imwrite('sift_keypoints.jpg',img)
+#
+# img=cv.drawKeypoints(gray,kp,img,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# cv.imwrite('sift_keypoints.jpg',img)
+
+# sift = cv.SIFT_create()
+# kp, des = sift.detectAndCompute(gray,None)
 
