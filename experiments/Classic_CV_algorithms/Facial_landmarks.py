faaf4f09564d93d31660c749a930395bce6cac6b
Glebbs <glebrost2013@yandex.ru>
2021-09-11 21:54:43 +0300

    implement face alignment and moved funcs to utils

--- experiments/Classic_CV_algorithms/Facial_landmarks.py
+++ experiments/Classic_CV_algorithms/Facial_landmarks.py
@@ -1,34 +1,12 @@
 from imutils import face_utils
-import numpy as np
 import imutils
 import dlib
 import cv2
 
-def rect_to_bb(rect):
-    # take a bounding predicted by dlib and convert it
-    # to the format (x, y, w, h) as we would normally do
-    # with OpenCV
-    x = rect.left()
-    y = rect.top()
-    w = rect.right() - x
-    h = rect.bottom() - y
-    # return a tuple of (x, y, w, h)
-    return (x, y, w, h)
-
-def shape_to_np(shape, dtype="int"):
-    # initialize the list of (x, y)-coordinates
-    coords = np.zeros((68, 2), dtype=dtype)
-    # loop over the 68 facial landmarks and convert them
-    # to a 2-tuple of (x, y)-coordinates
-    for i in range(0, 68):
-        coords[i] = (shape.part(i).x, shape.part(i).y)
-    # return the list of (x, y)-coordinates
-    return coords
-
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('C:/Users/glebr/Desktop/shape_predictor_68_face_landmarks.dat')
 # load the input image, resize it, and convert it to grayscale
-image = cv2.imread('C:/Users/glebr/Desktop/pict_2.jpg')
+image = cv2.imread('C:/Users/glebr/Desktop/pict_5.jpg')
 image = imutils.resize(image, width=500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # detect faces in the grayscale image
