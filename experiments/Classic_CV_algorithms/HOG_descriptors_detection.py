d49d55eb2f0f71eba6b5548f14a6188556e2a322
Glebbs <glebrost2013@yandex.ru>
2021-09-12 15:42:39 +0300

    move func to utils

--- experiments/Classic_CV_algorithms/HOG_descriptors_detection.py
+++ experiments/Classic_CV_algorithms/HOG_descriptors_detection.py
@@ -1,35 +1,10 @@
-import cv2
-import numpy as np
-
-import argparse
+from utils.utils import convert_and_trim_bb
 import imutils
 import time
 import dlib
 import cv2
-import matplotlib as plt
-
 
 ################################## HOG + SVM ############################
-
-def convert_and_trim_bb(image, rect):
-    # extract the starting and ending (x, y)-coordinates of the
-    # bounding box
-    startX = rect.left()
-    startY = rect.top()
-    endX = rect.right()
-    endY = rect.bottom()
-    # ensure the bounding box coordinates fall within the spatial
-    # dimensions of the image
-    startX = max(0, startX)
-    startY = max(0, startY)
-    endX = min(endX, image.shape[1])
-    endY = min(endY, image.shape[0])
-    # compute the width and height of the bounding box
-    w = endX - startX
-    h = endY - startY
-    # return our bounding box coordinates
-    return (startX, startY, w, h)
-
 
 # load dlib's HOG + Linear SVM face detector
 print("[INFO] loading HOG + Linear SVM face detector...")
